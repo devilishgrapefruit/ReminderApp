f7f6a0ead2545cc61cab47d36c6ab8fce58fb16b
Alexinet <88674225+Alexinet@users.noreply.github.com>
2022-10-21 12:53:32 +0300

    Add funcionality to calculator + Refactoring
    Добавление функционала для функции calculator. Проведение рефакторинга кода.

--- Test_calculator.py
+++ Test_calculator.py
@@ -1,18 +1,19 @@
 import unittest
+import Reminder
 
 
 class Test_calculator(unittest.TestCase):
-  
-   #Тест-кейс на корректный расчет времени (строго относительно указанного времени во время написания теста)
+
+    #Тест-кейс на корректный расчет времени (строго относительно указанного времени во время написания теста)
     def test_correct_calc(self):
         self.calculateTim = Reminder.Reminder.calculateTime(12, 12, 12, 12)
         self.assertEqual(self.calculateTim(), "Уведомление сработает через: 52 дней 11 часа 23 минут")
+
 
     # Тест-кейс на некоррекный ввод (передачу) данных для расчета (указан 13 месяц)
     def test_wrong_calc(self):
         self.calculateTim = Reminder.Reminder.calculateTime(13, 11, 11, 11)
         self.assertRaises(self.calculateTim(), ValueError)
-
 
 
 if __name__ == '__main__':
